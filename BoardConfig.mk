b47fa4bb315aef0614e7329f0248f785c3fb8c5a
bigbiff bigbiff <bigbiff@teamw.in>
2014-02-23 14:41:38 -0500

    modified for screen timeout
    Change-Id: I6a8f7c97b3668dee5c0d3199878af6a21503b76c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,7 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 
 TARGET_TOUCHBOOST_FREQUENCY:= 1200
+TARGET_LCD_BLANK_PATH := /sys/devices/f9967000.i2c/i2c-0/0-0038/backlight/lm3630/bl_power
 
 -include vendor/lge/hammerhead/BoardConfigVendor.mk
 
@@ -124,7 +125,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 # The real path for this is /sys/devices/mdp.0/qcom,cmdss_fb_primary.160/leds/lcd-backlight/brightness but the comma doesn't compile correctly
 TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.160/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
-TW_NO_SCREEN_TIMEOUT := true
+#TW_NO_SCREEN_TIMEOUT := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
 # Enable workaround for slow rom flash
